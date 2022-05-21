b05b29ed2ab3fdd24fcaebf4f1485a95ea308ac1
Regan-Koopmans <regan.koopmans@teampicnic.com>
2022-05-21 15:11:28 +0200

    Add browser test suite

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 use wasm_bindgen::prelude::*;
+use web_sys::AudioBuffer;
 
 #[wasm_bindgen]
 extern {
-    pub fn alert(s: &str);
+    // By default it appears that println! does not yield a log line in the browser,
+    // so we will opt to reuse `console.log`
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn analyse_audio(buffer: AudioBuffer) {
+    log(format!("Loaded an AudioBuffer with a sample rate of {}", buffer.sample_rate()).as_str());
 }
